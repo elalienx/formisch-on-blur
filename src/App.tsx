a6caa2e6aa933a7cd0ff28917b77d0ae1a3a6f66
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-03-15 10:23:21 +0100

    added explanation

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,11 @@ export default function App() {
   return (
     <div id="App">
       <h1>This is a Sample Form Page</h1>
+      <p>The goal is to validate the fields on blur instead of typing.</p>
+      <p>
+        I have manage to make it work with the first field but the 2nd one
+        behaves closer to validate on input rather than on blur.
+      </p>
       <Form of={form} onSubmit={onSubmit}>
         <InputField id="name" label="Full name" placeholder="Jhon Smith" />
         <InputField id="email" label="E-mail" placeholder="jhon@email.com" />
