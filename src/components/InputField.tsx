cdfbd3629d4d239761afb38e7663dc1e9a09d832
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2026-03-15 11:55:23 +0100

    adding logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,7 @@
 import { useField, type FormStore } from "@formisch/react";
 
 import "../styles/input-field.css";
+import { useEffect, useState, type FocusEvent } from "react";
 
 interface Props {
   form: FormStore;
@@ -8,6 +9,8 @@ interface Props {
   label: string;
   placeholder?: string;
 }
+
+type UIState = "default" | "focus" | "error" | "success";
 
 export default function InputField({
   form,
@@ -17,13 +20,44 @@ export default function InputField({
 }: Props) {
   // State
   const field = useField(form, { path: [id] });
+  const [uiState, setUIState] = useState<UIState>("default");
+
+  // Methods
+  useEffect(() => {
+    if (form.isSubmitted && !field.isValid) {
+      setUIState("error");
+    }
+  }, [form.isSubmitted, field.isValid]);
+
+  function onBlur(event: FocusEvent<HTMLInputElement>) {
+    field.props.onBlur(event);
+
+    if (field.isDirty) {
+      console.log("running validation...");
+
+      if (field.isValid) {
+        console.log("field is valid ✅");
+        setUIState("success");
+      } else {
+        console.log("field is invalid ❌");
+        setUIState("error");
+      }
+    }
+  }
 
   return (
     <fieldset className="input-field">
       <label htmlFor={id}>{label}</label>
-      <input {...field.props} id={id} type="text" placeholder={placeholder} />
-      {field.errors && (
-        <small className="validation-message">{field.errors[0]}</small>
+      <input
+        {...field.props}
+        id={id}
+        className={uiState}
+        type="text"
+        placeholder={placeholder}
+        onBlur={onBlur}
+      />
+      {uiState === "error" && (
+        <small className="validation-message">{field.errors?.[0]}</small>
       )}
     </fieldset>
   );
